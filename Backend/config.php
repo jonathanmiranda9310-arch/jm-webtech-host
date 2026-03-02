bb0207176bfbf9df9249361102f5a19addc60597
Jonathan Miranda <jonathanmiranda9310@gmail.com>
2026-03-02 16:34:05 -0300

    Obscure database credentials in config.php
    Replaced sensitive database credentials with placeholders.

--- Backend/config.php
+++ Backend/config.php
@@ -1,9 +1,9 @@
 <?php
-$host = 'sql301.infinityfree.com';
-$db   = 'if0_41237905_Banco'; 
-$user = 'if0_41237905';
-$pass = 'pP6QQRGZt5kx';
+$host = xxxxxx;
+$db   = xxxxx; 
+$user = xxxxx;
+$pass = xxxxx;
 
-$admin_email = 'admin@jmwebtech.com';
-$admin_pass  = 'bemvindoaojmhost';
+$admin_email = 'xxxxxx;
+$admin_pass  = 'xxxxxx;
 ?>
